04cd55926a1b3eabdf9d264c788b4e2086e10a96
Muhammad Abubakar Amin Chaudhary <chaudharyabubakar702@gamil.com>
2026-04-28 17:36:55 +0500

    add-thing

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/", include("dispatch.urls")),
+    # Silence favicon.ico 404 logs
+    path("favicon.ico", lambda x: HttpResponse(status=204)),
 ]
-
-
