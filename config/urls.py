a9f724567909b0f286e3350a2b9ab6b70abe6c54
Muhammad Abubakar Amin Chaudhary <chaudharyabubakar702@gamil.com>
2026-04-28 18:50:03 +0500

    add-versal

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from django.http import HttpResponse
 
 urlpatterns = [
+    path("", lambda x: HttpResponse("Backend is running!")),
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/", include("dispatch.urls")),
